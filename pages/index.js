02ff272c0910ed7902f8a8c8a67cdf248a0d60f1
Kota Kanazawa <kota0818@gmail.com>
2022-02-23 12:36:56 +0900

    プロフィール文章修正

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,9 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          夫&2児の父 / Software Engineer / Ruby, React, GraphQL, Next.js /
+          Hirakata.rb主催 / NBA / ボディビル
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
