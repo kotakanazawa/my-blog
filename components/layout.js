c825b91541938da8a3d651ed62857ceeeeb6719f
Kota Kanazawa <kota0818@gmail.com>
2022-02-23 12:30:14 +0900

    大まかなブログの完成

--- components/layout.js
+++ components/layout.js
@@ -26,37 +26,17 @@ export default function Layout({ children, home }) {
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
-        {home ? (
+        {home && (
           <>
             <Image
               priority
               src="/images/profile.jpg"
               className={utilStyles.borderCircle}
-              height={144}
-              width={144}
+              height={100}
+              width={100}
               alt={name}
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <Image
-                  priority
-                  src="/images/profile.jpg"
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt={name}
-                />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
-              </Link>
-            </h2>
           </>
         )}
       </header>
---
+++ pages/posts/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head"
+import Layout from '../../components/layout.js'
+
+export default function Posts() {
+  return (
+    <Layout>
+      <Head>
+        <title>Posts</title>
+      </Head>
+      <h1>Here comes all the posts</h1>
+    </Layout>
+  )
+}
