4f6c6e8ac4817bb94e78e4ca678b153f06cdac32
Kota Kanazawa <kota0818@gmail.com>
2022-02-25 18:29:15 +0900

    個別記事にSEO設定を追加

--- components/layout.js
+++ components/layout.js
@@ -16,12 +16,6 @@ export default function Layout({ children, home }) {
           name="Kota Kanazawa"
           content="A portfolio site by Kota Kanazawa"
         />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
