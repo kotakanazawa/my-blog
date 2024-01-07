98de55a145a4595f78a28abeb41875f66251144e
Kota Kanazawa <kota0818@gmail.com>
2024-01-07 16:27:47 +0900

    Remove profile image and name from layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,28 +1,14 @@
-import Image from "next/image"
 import styles from "./layout.module.css"
 import utilStyles from "../styles/utils.module.css"
 import Link from "next/link"
 
-const name = "Kota Kanazawa"
 export const siteTitle = "Kota Kanazawa"
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-        {home && (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={100}
-              width={100}
-              alt={name}
-            />
-            <h1 className={utilStyles.headingXl}>{name}</h1>
-          </>
-        )}
+        <h1 className={utilStyles.headingXl}>Kota Kanazawa's blog</h1>
       </header>
       <main>{children}</main>
       {!home && (
