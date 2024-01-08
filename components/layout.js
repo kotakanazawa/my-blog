800f916c7c019d171fab843ddb6b09f28ece453e
Kota Kanazawa <kota0818@gmail.com>
2024-01-08 12:17:31 +0900

    Remove unused siteTitle variable

--- components/layout.js
+++ components/layout.js
@@ -2,8 +2,6 @@ import styles from "./layout.module.css"
 import utilStyles from "../styles/utils.module.css"
 import Link from "next/link"
 
-export const siteTitle = "Kota Kanazawa"
-
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
